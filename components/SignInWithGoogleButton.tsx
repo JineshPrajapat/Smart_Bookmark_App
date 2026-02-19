f5d26661c0eca2c6ac6c631cbb9fdf1686588def
JineshPrajapat <prajapatjinesh585@gmail.com>
2026-02-19 16:16:02 +0530

    Fix: Prevent multiple OAuth triggers and improve auth redirection feedback

--- components/SignInWithGoogleButton.tsx
+++ components/SignInWithGoogleButton.tsx
@@ -1,24 +1,54 @@
 'use client'
 import { supabase } from '@/lib/supabaseClient'
+import { useState } from 'react'
+import { Chrome } from 'lucide-react'
+import Loader from './Loader'
 
 export default function SignInWithGoogleButton() {
+  const [isRedirecting, setIsRedirecting] = useState<boolean>(false)
+
   const login = async () => {
-    const res = await supabase.auth.signInWithOAuth({
-      provider: 'google',
-      options: {
-        redirectTo: `${location.origin}/auth/callback`,
-      },
-    })
+    try {
+      setIsRedirecting(true)
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'google',
+        options: {
+          redirectTo: `${location.origin}/auth/callback`,
+        },
+      })
 
+      if (error)
+        throw error
+    }
+    catch (error) {
+      console.error('Login error:', error)
+    }
+    finally{
+      setIsRedirecting(false)
+    }
   }
-
 
   return (
     <button
       onClick={login}
-      className="bg-blue-600 text-white px-4 py-2 rounded-full cursor-pointer hover:scale-110 duration-300 hover:shadow-blue-900"
+      disabled={isRedirecting}
+      className={`
+        relative flex items-center justify-center gap-3 px-8 py-3 
+        rounded-2xl font-bold text-sm tracking-wide transition-all duration-300
+       bg-blue-600 disabled:bg-blue-200 text-white hover:bg-blue-700 hover:shadow-xl hover:shadow-blue-200 active:scale-95 cursor-pointer disabled:cursor-not-allowed'
+      `}
     >
-      Sign in with Google
+      {isRedirecting ? (
+        <>
+          <Loader size={18} />
+          <span>Redirecting to Google...</span>
+        </>
+      ) : (
+        <>
+          <Chrome size={18} />
+          <span>Continue with Google</span>
+        </>
+      )}
     </button>
   )
 }
